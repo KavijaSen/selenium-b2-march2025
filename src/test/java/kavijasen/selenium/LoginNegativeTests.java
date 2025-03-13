05523fc0be48dc0318f7d113dbe447c4c1ad4d24
KavijaSen <kavijaseneviratne@gmail.com>
2025-03-14 00:44:08 +1030

    Added LoginNegativeTests.java

--- src/test/java/kavijasen/selenium/LoginNegativeTests.java
+++ src/test/java/kavijasen/selenium/LoginNegativeTests.java
@@ -26,6 +26,7 @@ public class LoginNegativeTests {
         driver.quit();
     }
 
+    /**Test with valid credentials**/
     @Test
     public void testLoginWithValidCredentials()
     {
@@ -34,5 +35,38 @@ public class LoginNegativeTests {
         driver.findElement(By.id("login-button")).click();
 
     }
+
+    /**Test with valid username & invalid password**/
+    @Test
+    public void testLoginWithInvalidPassword()
+    {
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement((By.id("password")).sendKeys("Invalid"));
+        driver.findElement(By.id("login-button")).click();
+
+        String actualTitle = getPageTitle();
+        Assert.assertEquals(actualTitle, "Products", "Login Failed: Unexpected page Title");
+    }
+
+    /**Test with locked out user**/
+    @Test
+    public void testLockedOutUser()
+    {
+        driver.findElement(By.id("user-name")).sendKeys("locked_out_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+
+        String errorMessage = getErrorMessage();
+        Assert.assertEquals(errorMessage, "Epic sadface: Sorry, this user has been locked out.", "Login Failed: Unexpected error message");
+    }
+
+    /**Test with blank username**/
+    @Test
+    public void testBlankUsername()
+    {
+        enterCredentials("","secret_sauce");
+
+    }
+
 
 }

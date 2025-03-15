a027b0d7d474f5803a643315806e8db83c9d7f69
KavijaSen <kavijaseneviratne@gmail.com>
2025-03-15 12:34:45 +1030

    Added negative test cases for login in Selenium

--- src/test/java/kavijasen/selenium/LoginNegativeTests.java
+++ src/test/java/kavijasen/selenium/LoginNegativeTests.java
@@ -26,6 +26,28 @@ public class LoginNegativeTests {
         driver.quit();
     }
 
+
+
+    /**Method to enter username & password**/
+    public  void enterCredentials (String userName, String password)
+    {
+        WebElement usernameInput=driver.findElement(By.id("user-name"));
+        WebElement passwordInput=driver.findElement(By.id("password"));
+
+        usernameInput.clear();
+        usernameInput.sendKeys(userName);
+
+        passwordInput.clear();
+        passwordInput.sendKeys(password);
+    }
+
+
+
+    private String getErrorMessage() {
+        return driver.findElement(By.cssSelector("[data-test='error']")).getText();
+    }
+
+
     /**Test with valid credentials**/
     @Test
     public void testLoginWithValidCredentials()
@@ -41,11 +63,11 @@ public class LoginNegativeTests {
     public void testLoginWithInvalidPassword()
     {
         driver.findElement(By.id("user-name")).sendKeys("standard_user");
-        driver.findElement((By.id("password")).sendKeys("Invalid"));
+        driver.findElement(By.id("password")).sendKeys("Invalid");
         driver.findElement(By.id("login-button")).click();
 
-        String actualTitle = getPageTitle();
-        Assert.assertEquals(actualTitle, "Products", "Login Failed: Unexpected page Title");
+        String errorMessage = getErrorMessage();
+        Assert.assertEquals(errorMessage, "Epic sadface: Username and password do not match any user in this service","Login Failed: Unexpected error message");
     }
 
     /**Test with locked out user**/
@@ -60,13 +82,69 @@ public class LoginNegativeTests {
         Assert.assertEquals(errorMessage, "Epic sadface: Sorry, this user has been locked out.", "Login Failed: Unexpected error message");
     }
 
-    /**Test with blank username**/
+    /**Test with blank username, correct password**/
     @Test
     public void testBlankUsername()
     {
         enterCredentials("","secret_sauce");
+        String errorMessage = getErrorMessage();
+        Assert.assertEquals(errorMessage, "Epic sadface: Username is required", "Login Failed: Unexpected error message");
 
     }
 
+    /**Test with Invalid username correct password**/
+    @Test
+    public void testInvalidUsername()
+    {
+        enterCredentials("User1","secret_sauce");
+        String errorMessage= getErrorMessage();
+        Assert.assertEquals(errorMessage,"Epic sadface: Username and password do not match any user in this service","Login Failed: Unexpected error message");
+    }
+
+    /**Test with invalid username & invalid password**/
+    @Test
+    public void testInvalidNamePswrd()
+    {
+        enterCredentials("User", "password");
+        String errorMessage = getErrorMessage();
+        Assert.assertEquals(errorMessage, "Epic sadface: Username and password do not match any user in this service", "Login Failed: Unexpected error message");
+    }
+
+    /**Test with correct username blank password**/
+    @Test
+    public void testBlankPassword()
+    {
+        enterCredentials("standard_user","");
+        String errorMessage=getErrorMessage();
+        Assert.assertEquals(errorMessage, "Epic sadface: Password is required","Login Failed: Unexpected error message" );
+    }
+
+    /**Test with blank username & password**/
+    @Test
+    public void blankUsernamePswrd()
+    {
+        enterCredentials("","");
+        String errorMessage=getErrorMessage();
+        Assert.assertEquals(errorMessage,"Epic sadface: Username is required","Login Failed: Unexpected error message");
+    }
+
+    /**Special characters in username/password**/
+    @Test
+    public void charactersUnamePswrd()
+    {
+        enterCredentials("user@1223!","password@#1");
+        String errorMessage=getErrorMessage();
+        Assert.assertEquals(errorMessage, "Epic sadface: Username and password do not match any user in this service", "Login Failed: Unexpected error message");
+    }
+
+    /**Long password & username**/
+    @Test
+    public void longUnamePswrd()
+    {
+        enterCredentials("User123SeneviratneKavija","passwordKavijaSeneviratne123");
+        String errorMessage=getErrorMessage();
+        Assert.assertEquals(errorMessage,"Epic sadface: Username and password do not match any user in this service", "Login Failed: Unexpected error message");
+
+    }
 
 }

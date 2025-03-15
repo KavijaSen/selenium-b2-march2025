7531f19c5206defa4169b0fe3ea7e6c38787899c
KavijaSen <kavijaseneviratne@gmail.com>
2025-03-15 17:55:43 +1030

    Added negative test cases for login in Selenium

--- src/test/java/kavijasen/selenium/LoginNegativeTests.java
+++ src/test/java/kavijasen/selenium/LoginNegativeTests.java
@@ -16,6 +16,7 @@ public class LoginNegativeTests {
     public void beforeMethod()
     {
         driver=new ChromeDriver();
+        driver.manage().window().maximize();
         driver.get("https://www.saucedemo.com/");
     }
 

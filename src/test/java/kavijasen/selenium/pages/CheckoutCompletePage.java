b4b3dce2b9612ab03250d53f8dad4b957a0f61b2
KavijaSen <kavijaseneviratne@gmail.com>
2025-04-12 15:08:55 +0930

    Add CheckoutCompletePage

--- src/test/java/kavijasen/selenium/pages/CheckoutCompletePage.java
+++ src/test/java/kavijasen/selenium/pages/CheckoutCompletePage.java
@@ -1,4 +1,34 @@
 package kavijasen.selenium.pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import kavijasen.selenium.util.ActionBot;
+
 public class CheckoutCompletePage {
+
+    private final WebDriver driver;
+    private final ActionBot actionBot;
+
+    // "Thank you for your order!" message
+    private final By completeHeader = By.cssSelector("h2.complete-header[data-test='complete-header']");
+
+    public CheckoutCompletePage(WebDriver driver, ActionBot actionBot) {
+        this.driver = driver;
+        this.actionBot = actionBot;
+    }
+
+    // Method for order completion message
+    public boolean isOrderCompleteMessageDisplayed() {
+        return driver.findElement(completeHeader).isDisplayed();
+    }
+
+    // Check for the text of the completion message
+    public String getOrderCompleteMessage() {
+        return driver.findElement(completeHeader).getText();
+    }
+
+    // Get the current URL
+    public String getCurrentUrl() {
+        return driver.getCurrentUrl();
+    }
 }

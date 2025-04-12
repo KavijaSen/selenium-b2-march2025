eefb91c509efd20d009fa43c90aa3c284301fcc6
KavijaSen <kavijaseneviratne@gmail.com>
2025-04-12 15:07:11 +0930

    Add CartPage

--- src/test/java/kavijasen/selenium/pages/CartPage.java
+++ src/test/java/kavijasen/selenium/pages/CartPage.java
@@ -1,4 +1,43 @@
 package kavijasen.selenium.pages;
 
+import kavijasen.selenium.util.ActionBot;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 public class CartPage {
+
+    private final WebDriver driver;
+    private final ActionBot actionBot;
+
+    private final By cartIcon = By.className("shopping_cart_link");
+    private final By continueShoppingButton = By.id("continue-shopping");
+    private final By checkoutButton = By.id("checkout");
+
+    public CartPage(WebDriver driver, ActionBot actionBot) {
+        this.driver = driver;
+        this.actionBot = actionBot;
+    }
+
+    // Method to navigate to the Cart Page by clicking on the cart icon
+    public void goToCart() {
+        actionBot.waitAndClick(cartIcon);
+    }
+
+    // Method to get the page URL (Cart page)
+    public String getCurrentUrl() {
+        return driver.getCurrentUrl();
+    }
+
+    // Method to click the Continue Shopping button
+    public void clickContinueShopping() {
+        actionBot.waitAndClick(continueShoppingButton);
+    }
+
+
+    // Method to click the Checkout button
+    public void clickCheckout() {
+        actionBot.waitAndClick(checkoutButton);
+    }
+
+
 }
